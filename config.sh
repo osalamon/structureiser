09c61659cf96cc6cccf7dcb4dac07c46de50de57
osalamon <ondrej.salamon@vsb.cz>
2026-07-24 15:37:24 +0200

    chores: dir structure, fill in config.sh, git ignored of appropriate files

--- config.sh
+++ config.sh
@@ -5,14 +5,14 @@
 # Otherwise keep it local to the script that sources this file.
 
 # ---- paths -------------------------------------------------------
-export SCRIPT_DIR="${HOME}/structureiser"
-export SINGULARITY_DIR="${SCRIPT_DIR}/singularity"
+export SCRIPT_DIR="${HOME}/OPENsalam/WP3/structureiser"
+export SINGULARITY_DIR="${SCRIPT_DIR}/apptainer"
 
 # ---- tooling versions -------------------------------------------
-export SINGULARITY_IMAGE_NAME="structureiser"
+export SINGULARITY_IMAGE_NAME="apptainer"
 
 # ---- evaluation -------------------------------------------------
-export LILI_DIR="${SCRIPT_DIR}/data/Lili/"
+export LILI_DIR="${SCRIPT_DIR}/data/highway-crash/"
 export CHECKPOINT_DIR="${LILI_DIR}/checkpoints/"
 export CHECKPOINT_FILENAME="checkpoint_best.pth"
 export OUTPUT_DIR="${LILI_DIR}/stylized/"
