91b58b79f75cb0824392a3c3b831def0d2b1c70a
osalamon <ondrej.salamon@vsb.cz>
2026-09-07 14:38:35 +0200

    FP precision halved to have a quick test run

--- train.py
+++ train.py
@@ -573,6 +573,7 @@ def train(config, model, iters, key_weight, style_weight, structure_weight, data
     params_to_optimize = list(model.parameters())
 
     optimizer = opt.AdamW(params_to_optimize, lr=3e-5)  # RMSprop works at 2e-4
+    scaler = torch.cuda.amp.GradScaler()
     aux_sample = InfiniteDatasetSampler(dataset_aux)
     ebest = float('inf')
 
@@ -628,13 +629,13 @@ def train(config, model, iters, key_weight, style_weight, structure_weight, data
 
                 optimizer.zero_grad()
 
-                with suppress():
+                with torch.cuda.amp.autocast():
                     y = model(keyframe_x.clone())
 
                 # L1 Loss Calculation
                 key_loss += key_weight * image_loss(y, keyframe_y)
 
-                with suppress():
+                with torch.cuda.amp.autocast():
                     frame_y = model(frame_x.clone())
 
                 with suppress():
@@ -651,8 +652,12 @@ def train(config, model, iters, key_weight, style_weight, structure_weight, data
                 scalars = {name: value for name, value in locals().items() if name in tracked_scalars}
                 log.log_multiple_scalars(scalars, epoch)
 
-                error.backward()
-                optimizer.step()
+                # error.backward()
+                # optimizer.step()
+
+                scaler.scale(error).backward()                                                                                                                                      
+                scaler.step(optimizer)                                                                                                                                              
+                scaler.update() 
 
                 trange.set_postfix({'err': f'{error:0.5f}',
                                     'key': f'{key_loss:0.5f}',
